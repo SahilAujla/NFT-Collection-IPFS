ed28694c2e9cd5dac1ef1315e9cdd85eef98501f
Sahil Aujla <sahilaujla15@gmail.com>
2022-03-22 06:09:52 +0530

    Done with the frontend

---
+++ smart-contracts/scripts/deploy.js
@@ -0,0 +1,30 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  // URL from where we can extract the metadata for a LW3Punks
+  const metadataURL = "ipfs://Qmbygo38DWF1V8GttM1zy89KzyZTPU2FLUzQtiDvB7q6i5/";
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so lw3PunksContract here is a factory for instances of our LW3Punks contract.
+  */
+  const lw3PunksContract = await ethers.getContractFactory("LW3Punks");
+
+  // deploy the contract
+  const deployedLW3PunksContract = await lw3PunksContract.deploy(metadataURL);
+
+  await deployedLW3PunksContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("LW3Punks Contract Address:", deployedLW3PunksContract.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+// contract address - 0x3A621355F6eca645eE8E3313F99e88A1CFf4F9e0
